e2a46706e51e3250786678b0bac97014038aaa22
Jay Deokar <jsdeokar@amazon.com>
2023-09-11 13:28:20 -0700

    Update scripts to run cyclonus suite and install latest MAO

--- scripts/lib/cleanup.sh
+++ scripts/lib/cleanup.sh
@@ -6,19 +6,21 @@ function check_path_cleanup(){
     do
         export NODE=$node
         envsubst '$NODE' < ${DIR}/test/check-cleanup-pod.yaml > ${DIR}/test/check-cleanup-pod-$node.yaml
-        kubectl apply -f ${DIR}/test/check-cleanup-pod-$node.yaml
+        kubectl apply -f ${DIR}/test/check-cleanup-pod-$node.yaml -n default
         rm -rf ${DIR}/test/check-cleanup-pod-$node.yaml
     done
     sleep 20
 
     for node in $worker_nodes
     do
-        if [[ $(kubectl get pods $node -ojsonpath="{.status.phase}") == "Failed" ]]; then
-            echo "BPF files not cleaned up on $node.. $(kubectl logs $node)"
-            exit 1
+        if [[ $(kubectl get pods -n default $node -ojsonpath="{.status.phase}") == "Failed" ]]; then
+            echo "BPF files not cleaned up on $node"
+            kubectl logs $node -n default
+            TEST_FAILED=true
+        else
+            echo "BPF files were cleaned up from the node $node"
         fi
-        kubectl delete pods $node
+        kubectl delete pods $node -n default
     done
 
-    echo "BPF files were cleaned up from the nodes"
 }
